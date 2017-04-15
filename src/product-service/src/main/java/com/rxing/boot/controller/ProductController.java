e3808ee51541f28fdde67721b0f9b1ee65c0ad8b
wxh <wxh054510@sina.com>
2017-04-15 14:37:29 +0800

    修改数据库表结构及添加数据服务接口

--- src/product-service/src/main/java/com/rxing/boot/controller/ProductController.java
+++ src/product-service/src/main/java/com/rxing/boot/controller/ProductController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rxing.boot.dao.IProductDao;
+import com.rxing.boot.entity.Category;
 import com.rxing.boot.entity.Product;
 
 /**
@@ -97,8 +100,9 @@ public class ProductController {
 	 * @return
 	 */
 	@RequestMapping(value="page",method=RequestMethod.GET)
-	public Page<Product> findOne(@RequestParam int page,@RequestParam int size,@RequestParam List<String> properties){
-		Page<Product>  product=productDao.findAll(new MySpec(),new PageRequest(page, size,new Sort(Direction.DESC,properties)));		
+	public Page<Product> findOne(@RequestParam int page,@RequestParam int size,@RequestParam List<String> properties,
+			Product prod,Category cate){
+		Page<Product>  product=productDao.findAll(new MySpec(prod,cate),new PageRequest(page, size,new Sort(Direction.DESC,properties)));		
 		return product;
 	}
 	
@@ -112,16 +116,50 @@ public class ProductController {
 	}
 	
 	/**
+	 * 批量删除指定商品
+	 * @param  
+	 * @param products
+	 */
+	@RequestMapping(value="deleteBatch",method=RequestMethod.GET)
+	public void  deleteBatch(@RequestParam Iterable<Product> products){
+		productDao.delete(products);
+	}
+	
+	
+	/**
 	 * 自定义查询方式
 	 */
 	private class MySpec implements Specification<Product>{
+		
+		private Product product;
+		private Category category;
+		
+		public MySpec(Product product,Category category) {
+			this.product=product;
+			this.category=category;
+		}		
 		@Override
 		public Predicate toPredicate(Root<Product> root, CriteriaQuery<?> query, CriteriaBuilder cBuilder) {			
 			// 多条件分页查询
-			Path<String> exp1= root.get("productName");
-			Path<String> exp2=root.get("productRest");			 
-			query.where(cBuilder.like(exp1, "%ban%"),cBuilder.equal(exp2, "56"));		
-			return null;
+			
+			Path<Long> productId= root.get("productId");
+			Path<String> productName= root.get("productName");
+			Path<Float> productPrice=root.get("productPrice");
+			Path<Integer> productRest= root.get("productRest");
+			Path<Float> productRebate=root.get("productRebate");
+			Path<String> productClickCounts=root.get("productClickCounts");
+			
+			
+			query.where(cBuilder.equal(productId, product.getProductId()),
+					cBuilder.like(productName, "%"+product.getProductName()+"%"),
+					cBuilder.lessThan(productPrice, product.getProductPrice()),			
+					cBuilder.lessThan(productRest, product.getProductRest()),
+					cBuilder.lessThan(productRebate, product.getProductRebate()),
+					cBuilder.greaterThan(productClickCounts, String.valueOf(product.getProductClickCounts()))
+					);	
+			
+			Path<String> categoryName=root.join("category",JoinType.INNER).get("categoryName");
+			return cBuilder.like(categoryName, "%"+category.getCategoryName()+"%");
 		}
 	}
 	
